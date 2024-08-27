f782b90d17bd5125c9f87ca29dd9bf356636a339
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-08-27 21:55:55 +0530

    LA1:2

--- Lab/Assignment-1/q2.m
+++ Lab/Assignment-1/q2.m
@@ -5,9 +5,47 @@ close all;
 
 %% Permanent Magnet DC Motor (PMDC) system
 % Constants
-Ra = 0.1; % [Ohm]
-La = 0.01; % [H]
+R_a = 0.1; % [Ohm]
+L_a = 0.01; % [H]
 J = 10; % [kgm^2]
 B = 0.01; % [Nms]
-kb = 1; % [Vs/rad]
-kt = 1; % [Nm/A]
+k_b = 1; % [Vs/rad]
+k_t = 1; % [Nm/A]
+tspan = 90; % Time span [s]
+T_load = 0; % Load torque [Nm]
+V_inp = 20; % Input voltage [V]
+x0 = [0; 0]; % Initial state value
+
+h = 0.01; % Step size
+tsamples = (0:h:tspan)'; % Time samples
+numSamples = length(tsamples);
+X = zeros(2, numSamples);
+
+% State space representation
+f = @(t, x) [(1 / L_a) * (V_inp - R_a * x(1) - k_b * x(2)); ...
+                 (1 / J) * (k_t * x(1) - B * x(2) - T_load)];
+
+%% Euler's method
+% Implementation
+tic
+
+for i = 1:numSamples - 1
+    X(:, i + 1) = X(:, i) + h * f(tsamples(i), X(:, i));
+end
+
+toc
+
+%% Plots
+subplot(2, 1, 1);
+plot(tsamples, X(1, :), "b", "DisplayName", "Current i_a");
+ylabel("Current i_a(t)");
+xlabel("Time (seconds)");
+title('Current vs Time');
+legend();
+
+subplot(2, 1, 2);
+plot(tsamples, X(2, :), "b", "DisplayName", "Speed w");
+ylabel("Speed w(t)");
+xlabel("Time (seconds)");
+title('Speed vs Time');
+legend();
