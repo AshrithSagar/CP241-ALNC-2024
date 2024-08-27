e1441cb18e648fa47ac1d0dfbf6889152326bb22
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-08-27 10:03:51 +0530

    LA1:1

--- Lab/Assignment-1/q1.m
+++ Lab/Assignment-1/q1.m
@@ -1,11 +1,25 @@
 %% ALNC Lab Assignment-1 Question-1
 clc;
-clear all;
+clear;
 close all;
 
 %% Simple pendulum system
-% Constants
-g = 9.81;  % Acceleration due to gravity
-l = 0.1;  % Length of the pendulum
-m = 0.01;  % Mass of the pendulum
-b = 0.01;  % Damping coefficient
+% Parameters
+g = 9.81; % Acceleration due to gravity
+l = 0.1; % Length of the pendulum
+m = 0.01; % Mass of the pendulum
+b = 0.01; % Damping coefficient
+t_end = 40; % [seconds]
+dt = 0.1;
+tsamples = (0:dt:t_end);
+
+% State initialisation
+numSamples = length(tsamples);
+X = zeros(1, numSamples);
+
+% Initial state value
+x0 = [pi / 6, pi / 12];
+
+% State space representation
+f1 = @(t, x) x(2);
+f2 = @(t, x) -g / l * sin(x(1)) - b / (m * l * l * x(2));
