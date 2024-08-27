6f10b98c37afbccdff4b219ea3a4d6d1ac8aaf7e
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-08-27 08:05:54 +0530

    LA1:1

---
+++ Lab/Assignment-1/q1.m
@@ -0,0 +1,11 @@
+%% ALNC Lab Assignment-1 Question-1
+clc;
+clear all;
+close all;
+
+%% Simple pendulum system
+% Constants
+g = 9.81;  % Acceleration due to gravity
+l = 0.1;  % Length of the pendulum
+m = 0.01;  % Mass of the pendulum
+b = 0.01;  % Damping coefficient
