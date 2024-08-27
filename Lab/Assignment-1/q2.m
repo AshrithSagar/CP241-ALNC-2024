3a1ec9bc08b6ed1aa6da24c2ebb4233885063650
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-08-27 08:11:51 +0530

    LA1:2

---
+++ Lab/Assignment-1/q2.m
@@ -0,0 +1,13 @@
+%% ALNC Lab Assignment-1 Question-2
+clc;
+clear all;
+close all;
+
+%% Permanent Magnet DC Motor (PMDC) system
+% Constants
+Ra = 0.1  % [Ohm]
+La = 0.01  % [H]
+J = 10  % [kgm^2]
+B = 0.01  % [Nms]
+kb = 1  % [Vs/rad]
+kt = 1  % [Nm/A]
