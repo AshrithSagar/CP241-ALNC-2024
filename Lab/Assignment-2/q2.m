2961e449e5f9c6efb6be79aa6a9698c47ad784b1
Ashrith Sagar <ashrith9sagar@gmail.com>
2024-09-09 08:10:29 +0530

    LA2:2

--- Lab/Assignment-2/q2.m
+++ Lab/Assignment-2/q2.m
@@ -13,20 +13,20 @@ X_0 = [0, 0, pi, 0]; % Initial state vector
 
 % Control input
 % S.R. No. : 24233
-function r = control_input(t, x)
+function u = control_input(t, x)
 
-    if t < 10
-        r = -2 * x(3);
-    elseif t < 20
-        r = -4 * x(3);
-    elseif t < 30
-        r = -2 * x(3);
-    elseif t < 40
-        r = -3 * x(3);
-    elseif t < 50
-        r = -3 * x(3);
+    if t >= 0 && t < 10
+        u = -2 * x(3);
+    elseif t >= 10 && t < 20
+        u = -4 * x(3);
+    elseif t >= 20 && t < 30
+        u = -2 * x(3);
+    elseif t >= 30 && t < 40
+        u = -3 * x(3);
+    elseif t >= 40 && t < 50
+        u = -3 * x(3);
     else
-        r = 0;
+        u = 0;
     end
 
 end
@@ -41,7 +41,7 @@ function f = system(t, x, u_func)
     b = 10; % Friction coefficient
     g = 9.8; % Acceleration due to gravity
 
-    u = u_func(t, x);
+    u = u_func(t, x); % Control input
     A = -b * x(2) - m * L * x(4) ^ 2 * sin(x(3)) + m * g * cos(x(3)) * sin(x(3)) + u;
     B = M + m * sin(x(3)) ^ 2;
 
@@ -55,21 +55,26 @@ end
 
 % Simulation using ode45
 [t, X] = ode45(@(t, x) system(t, x, @control_input), t, X_0);
-u = arrayfun(@(ti) control_input(ti, X(find(t == ti), :)), t);
+u = arrayfun(@(ti) control_input(ti, X(t == ti, :)), t);
 
 %% Plots
 figure;
-subplot(2, 1, 1);
 plot(t, u, 'r', 'LineWidth', 1.5, 'DisplayName', 'Control input');
+xlabel('Time [s]');
+ylabel('Control input');
+title('Control input vs Time');
 legend;
 grid on;
 
-subplot(2, 1, 2);
+figure;
 hold on;
 plot(t, X(:, 1), 'r', 'LineWidth', 1.5, 'DisplayName', 'x_1 [Position]');
-plot(t, X(:, 2), 'g', 'LineWidth', 1.5, 'DisplayName', 'x_2 [Velocity]');
+% plot(t, X(:, 2), 'g', 'LineWidth', 1.5, 'DisplayName', 'x_2 [Velocity]');
 plot(t, X(:, 3), 'b', 'LineWidth', 1.5, 'DisplayName', 'x_3 [Angle]');
-plot(t, X(:, 4), 'm', 'LineWidth', 1.5, 'DisplayName', 'x_4 [Angular velocity]');
+% plot(t, X(:, 4), 'm', 'LineWidth', 1.5, 'DisplayName', 'x_4 [Angular velocity]');
+xlabel('Time [s]');
+ylabel('States');
+title('States vs Time');
 legend;
 grid on;
 hold off;
